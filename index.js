997d03b90fc864557062e38c4a9587810d27831c
sofia ongele <sofiaongele@gmail.com>
2018-07-16 17:51:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+
+
 function searchRepositories(searchTerms) {
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
       $('#results').html(renderSearchResults(data))
