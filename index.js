c5714c3e819c6f049be4a82b0295f0b35532f7ad
sofia ongele <sofiaongele@gmail.com>
2018-07-16 17:49:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function searchRepositories(searchTerms) {
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
       $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
     })
 }
 
